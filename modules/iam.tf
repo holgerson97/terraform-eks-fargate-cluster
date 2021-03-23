f4bce910cde881f98a6ec17798e667c2bbeba4e8
NilsCarstensen <nilscarstensen@live.com>
2021-03-23 10:30:26 +0100

    add fargate profiles

--- modules/iam.tf
+++ modules/iam.tf
@@ -1,4 +1,4 @@
-data "aws_iam_policy_document" "assume_role" {
+data "aws_iam_policy_document" "assume_role_eks" {
 
     statement {
         effect  = "Allow"
@@ -16,11 +16,9 @@ resource "aws_iam_role" "default" {
 
     description          = "IAM role to manage the cluster and deploy fargate pods."
 
-    name                 = "eks-cluster"
-    assume_role_policy   = data.aws_iam_policy_document.assume_role.json
-    permissions_boundary = var.permissions-boundary
-
-    depends_on = [ aws_iam_policy_document.assume_role ]
+    name                 = var.eks_cluster_iam_role_name
+    assume_role_policy   = data.aws_iam_policy_document.assume_role_eks.json
+    permissions_boundary = var.permissions_boundary
 
 }
 
@@ -30,5 +28,59 @@ resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
   role       = aws_iam_role.default
 
   depends_on = [ aws_iam_role.default ]
+
+}
+
+data "aws_iam_policy_document" "assume_role_pod_execution" {
+
+    statement {
+        effect  = "Allow"
+        actions = ["sts:AssumeRole"]
+
+        principals {
+            type        = "Service"
+            identifiers = ["eks-fargate-pods.amazonaws.com"]
+        }
+    }
+
+}
+
+resource "aws_iam_role" "fargate_pod_execution" {
+
+    description          = "IAM role to allow fargate profiles to run pods inside EKS cluster."
+
+    name                 = var.fargate_iam_role_name
+    assume_role_policy   = data.aws_iam_policy_document.assume_role_pod_execution 
+    permissions_boundary = var.permissions_boundary
+
+}
+
+resource "aws_iam_role_policy_attachment" "example-AmazonEKSFargatePodExecutionRolePolicy" {
+
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSFargatePodExecutionRolePolicy"
+    role       = aws_iam_role.fargate_pod_execution
+
+    depends_on = [ aws_iam_role.fargate_pod_execution ]
+}
+
+variable "eks_cluster_iam_role_name" {
+
+    description = "Name of EKS cluster IAM role."
+
+    type        = string
+    default     = "eks_cluster"
+
+    sensitive   = false
+
+}
+
+variable "fargate_iam_role_name" {
+
+    description = "Name of fargate pod execution IAM role."
+
+    type        = string
+    default     = "fargate_pod_execution_role"
+
+    sensitive   = false
 
 }
