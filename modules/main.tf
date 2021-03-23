3e23666ea739984af5510022a80069ce5f32f09d
NilsCarstensen <nilscarstensen@live.com>
2021-03-23 10:42:05 +0100

    add fargate profiel for kube-system ns

--- modules/main.tf
+++ modules/main.tf
@@ -41,7 +41,7 @@ resource "aws_internet_gateway" "internet-gw" {
 
 }
 
-resource "aws_eks_cluster" "eks-cluster" {
+resource "aws_eks_cluster" "eks_cluster" {
 
     name     = var.eks-cluster-name
     version  = var.kubernetes_version
@@ -68,4 +68,25 @@ resource "aws_eks_cluster" "eks-cluster" {
 
     ]
 
+}
+
+resource "aws_eks_fargate_profile" "kube-system" {
+
+    cluster_name           = aws_eks_cluster.eks_cluster.name
+    fargate_profile_name   = "kube-system"
+    pod_execution_role_arn = aws_iam_role.fargate_pod_execution.arn
+    subnet_ids             = aws_subnet.cluster_subnets[*].id
+
+    selector {
+
+        namespace = "kube-system"
+        
+    }
+
+    depends_on = [
+
+        aws_eks_cluster.eks_cluster
+        aws_iam_role.fargate_pod_execution
+
+    ]
 }
