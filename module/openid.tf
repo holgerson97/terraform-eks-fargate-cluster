1f6550a07d91c38812ff6746e6780009dfc0de8e
NilsCarstensen <nilscarstensen@live.com>
2021-04-08 14:11:41 +0200

    Update: Connect provider clientid list

--- module/openid.tf
+++ module/openid.tf
@@ -3,45 +3,13 @@ variable "openid_enabled" {
     description = "Enable the OpenID Connect provider for Kubernetes."
     
     type        = bool
-    default     = false
+    default     = true
     
     sensitive   = false
   
 }
 
-variable "openid_url" {
-
-    description = "OpenID url where users login."
-    
-    type        = string
-    default     = null
-    
-    sensitive   = false
-
-}
-
-variable "openid_client_id" {
-
-    description = "Provider specific OpenID application specifier."
-    
-    type        = list(string)
-    default     = null
-    
-    sensitive   = false
-
-}
-
-variable "openid_thumbprint" {
-
-    description = "Provider specific OpenID server certificate thumbprints."
-    
-    type        = list(string)
-    default     = null
-    
-    sensitive   = false
-
-}
-
+# https://registry.terraform.io/providers/hashicorp/tls/latest/docs/data-sources/tls_certificate
 data "tls_certificate" "openid_tls" {
   
     url = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
@@ -53,9 +21,8 @@ resource "aws_iam_openid_connect_provider" "main" {
 
     count           = var.openid_enabled == true ? 1 : 0
 
-    client_id_list  = var.openid_client_id
-    url             = aws_eks_cluster.eks_cluster.identity[0].oidc[0].issuer
+    client_id_list  = [ "sts.amazonaws.com" ]
+    thumbprint_list = [ data.tls_certificate.openid_tls.certificates[0].sha1_fingerprint ]
+    url             = [ join("", aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer) ]
 
-    thumbprint_list = [data.tls_certificate.openid_tls.certificates[0].sha1_fingerprint]
-  
 }
