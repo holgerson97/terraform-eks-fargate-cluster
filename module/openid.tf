dddcc2e60b9f39a40a71c323d2a2afaa034215aa
Nils Carstensen <nilscarstensen@live.com>
2021-04-08 17:17:35 +0200

    FIx: openid provider connect url

--- module/openid.tf
+++ module/openid.tf
@@ -23,6 +23,6 @@ resource "aws_iam_openid_connect_provider" "main" {
 
     client_id_list  = [ "sts.amazonaws.com" ]
     thumbprint_list = [ data.tls_certificate.openid_tls.certificates[0].sha1_fingerprint ]
-    url             = [ join("", aws_eks_cluster.eks_cluster.identity.0.oidc.0.issuer) ]
+    url             = join("", aws_eks_cluster.eks_cluster.*.identity.0.oidc.0.issuer)
 
 }
